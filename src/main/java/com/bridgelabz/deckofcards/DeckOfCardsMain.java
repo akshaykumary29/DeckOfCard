4c7031cf26bd2b31526ca0e64a0328447ca1c298
akshay <akshay.yamgar29@gmail.com>
2021-11-02 08:50:41 +0530

    Added:Welcome Message.

---
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.deckofcards;
+
+/**
+ * Workshop on Deck Of Cards Game
+ */
+public class DeckOfCardsMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deck Of Cards Game.!!!");
+    }
+}
