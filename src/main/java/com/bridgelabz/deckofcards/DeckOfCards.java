7868dc86be856ca766834a846f30ef0f52e25927
akshay <akshay.yamgar29@gmail.com>
2021-11-02 09:15:42 +0530

    Added:Set up and Display Unique cards

--- src/main/java/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.deckofcards;
 
 public class DeckOfCards {
+    // Create the method for uniqueCards
+    public void uniqueCards() {
+        String[] cards = new String[52];
+        String[] suit = {"Club", "Diamond", "Heart", "Spade"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Ace", "King", "Queen", "Jack"};
+        int position = 0;
+        for (int i = 0; i < suit.length; i++) {
+            for (int j = 0; j < rank.length; j++) {
 
+                cards[position] = suit[i] + rank[j];
+                System.out.println(cards[position] = suit[i] + " " + "->" + " " + rank[j]);
+            }
+        }
+    }
 }
--- src/main/java/com/bridgelabz/deckofcards/DeckOfCardsMain.java
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCardsMain.java
@@ -2,9 +2,17 @@ package com.bridgelabz.deckofcards;
 
 /**
  * Workshop on Deck Of Cards Game
+ *
+ * @author : Akshay
+ * @version : 8.0
+ * @since : 02/11/2021
  */
 public class DeckOfCardsMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Deck Of Cards Game.!!!");
+        // create the object for PlayCards
+        DeckOfCards cardsPlay = new DeckOfCards();
+        // call to main method
+        cardsPlay.uniqueCards();
     }
 }
